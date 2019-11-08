97ff0a3bb2eea8f7051e7a8ed0ff2c27d465b86c
Essam-Harous <esamfarg91@gmail.com>
2019-11-09 01:54:31 +0200

    almost done

--- main.js
+++ main.js
@@ -4,17 +4,20 @@ var $stopBtn = $('#stop');
 var $resetBtn = $('#reset')
 var $minutes = $('#minutes')
 var $secs = $('#secs')
-var $fullPomodor = $('#fullPomodor');
+var $fullPomodoro = $('#fullPomodoro');
 var $pomodoroPart = $('#pomodoroPart');
 var pomodoro = 0;
+var fullPomodoro = 0
 var timeLeft = 0;
 var workTime = 0;
 var interval = 0;
 var firstClick = true;
-var breakTime = false
+var breakTime = false;
+
+var $chart = $('#chart');
 
 $startBtn.on('click', function(){
-	$stopBtn.removeAttr('disabled')
+	$stopBtn.removeAttr('disabled');
 	var minutes = $minutes.val();
 	var secs = $secs.val();
 	if (/[e.+-]/.test(minutes) || /[e.+-]/.test(secs) || '' === secs || '' === minutes) {
@@ -24,9 +27,10 @@ $startBtn.on('click', function(){
     $minutes.attr('disabled', 'true');
     $secs.attr('disabled', 'true');
     if (firstClick) {
-	    workTime = timeInSecs(minutes, secs);
+		workTime = timeInSecs(minutes, secs);
+		console.log(workTime, 'fsfs');
+		firstClick = false;
     }
-    firstClick = false;
     timeLeft = timeInSecs(minutes, secs);
 
     interval = setInterval(display, 1000);
@@ -44,21 +48,38 @@ function display() {
 		$minutes.val('00');
 		$secs.val('00')
 		$('#alarm')[0].play();
+		$chart.data('easyPieChart').update(0);
 		breakTime = !breakTime;
 		if (breakTime) {
 			pomodoro++;
+			if (pomodoro === 4) {
+				pomodoro = 0;
+				fullPomodoro++;
+				$fullPomodoro.text(fullPomodoro);
+				timeLeft = (workTime / 5 * 3);
+				$chart.data('easyPieChart').update(timeLeft * 100 / (workTime / 5 * 3))
+			}else {
+				timeLeft = (workTime / 5);
+				$chart.data('easyPieChart').update(timeLeft * 100 / (workTime / 5))
+			}
 			$pomodoroPart.text(pomodoro);
-			timeLeft = workTime / 5;
-			setTimeout(function() {return}, 6000)
 		}else {
 			timeLeft = workTime;
 		}
-
+	}else {
+		if (breakTime && pomodoro == 0) {
+			$chart.data('easyPieChart').update(timeLeft * 100 / (workTime / 5 * 3))
+		}else if(breakTime) {
+			$chart.data('easyPieChart').update(timeLeft * 100 / (workTime / 5))
+		}else {
+			$chart.data('easyPieChart').update(timeLeft * 100 / workTime)
+		}
 	}
-		var currentSecs = timeLeft % 60;
-		var currentMin = Math.floor(timeLeft / 60);
-		$minutes.val(currentMin < 10? '0' + currentMin: currentMin);
-		$secs.val(currentSecs < 10? '0' + currentSecs: currentSecs);
+
+	var currentSecs = timeLeft % 60;
+	var currentMin = Math.floor(timeLeft / 60);
+	$minutes.val(currentMin < 10? '0' + currentMin: currentMin);
+	$secs.val(currentSecs < 10? '0' + currentSecs: currentSecs);
 }
 
 $stopBtn.on('click', function() {
@@ -75,4 +96,15 @@ $resetBtn.on('click', function() {
     $minutes.removeAttr('disabled');
     $secs.removeAttr('disabled');
 	$stopBtn.removeAttr('disabled');
+	$chart.data('easyPieChart').update(100);
+	pomodoro = 0;
+	$pomodoroPart.text(0)
+	fullPomodoro = 0;
+	$fullPomodoro.text(0);
+	timeLeft = 0;
+	workTime = 0;
+	interval = 0;
+	firstClick = true;
+	breakTime = false;
+
 })
