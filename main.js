bf6902737fd7da83a2f2603a644e4c3fda0fb5a4
essam <esamfarg91@gmail.com>
2019-11-08 08:58:38 +0100

    good

--- main.js
+++ main.js
@@ -1,9 +1,22 @@
-var minutes = $('#minutes').text();
-var secs = $('#secs').text();
-var start = $('#start');
-start.on('click', function(){
+
+var start-btn = $('#start');
+var stop-btn = $('#stop')
+
+start-btn.on('click', function(){
+	var minutes = $('#minutes').text();
+	var secs = $('#secs').text();
     start.attr('disable', 'true');
-    console.log(minutes, secs)
-    alert(minutes + secs)
+
+    var interval = setInterval(startTimer(minutes, secs), 1000);
 
 })
+
+startTimer(minutes, secs) {
+
+	
+
+	if (secs > 0) {
+		secs--
+
+	}
+}
